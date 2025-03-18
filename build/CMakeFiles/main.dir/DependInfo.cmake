916d21a4b877ac7799a3a1122d862f77f6540950
Sam Fishburn <samfishburn@icloud.com>
2025-03-18 18:27:40 -0400

    Finished the CPU, untested.

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sam/NES_Emulator/Bus.cpp" "CMakeFiles/main.dir/Bus.cpp.o" "gcc" "CMakeFiles/main.dir/Bus.cpp.o.d"
+  "/home/sam/NES_Emulator/CPU.cpp" "CMakeFiles/main.dir/CPU.cpp.o" "gcc" "CMakeFiles/main.dir/CPU.cpp.o.d"
+  "/home/sam/NES_Emulator/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/Bus.cpp.o"
+  "CMakeFiles/main.dir/Bus.cpp.o.d"
+  "CMakeFiles/main.dir/CPU.cpp.o"
+  "CMakeFiles/main.dir/CPU.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
